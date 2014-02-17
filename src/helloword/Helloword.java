15082b4b050afbd10650fa40a4ee6538089bc1ae
Nour <Nour@Nour-PC>
2014-02-17 23:19:24 +0100

    hello

--- src/helloword/Helloword.java
+++ src/helloword/Helloword.java
@@ -14,6 +14,6 @@ public class Helloword {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("marwen");
+        System.out.println("Nour :D");
     }
 }
